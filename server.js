55029e61be5113eca232e74ed517f30086e842b7
ManditjyotiBorah <67102070+ManditjyotiBorah@users.noreply.github.com>
2021-03-01 21:17:17 +0530

    Update server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+//add middlewares here
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//add routes below
+
+// const sellerRoute = require('./routes/seller.route')
+// app.use('/seller', sellerRoute)
+
+const productRoute = require('./routes/product.route')
+app.use('/product',productRoute)
+
+
+//connection
+
+mongoose
+  .connect(process.env.DATABASE_ACCESS)
+  .then(() => {
+    app.listen(4000);
+    console.log(
+      "server running"
+    );
+  })
+  .catch((err) => console.log(err));
